a2a1024013e2b109ff95c36a3b1a18172728a06f
Justice Otuya <jotuya2@gmail.com>
2020-12-22 20:22:17 +0100

    add tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,23 @@ module.exports = {
                 path: `${__dirname}/src/images/`,
             },
         },
-        // {
-        //     resolve: 'gatsby-source-filesystem',
-        //     options: {
-        //         name: 'portfolio',
-        //         path: `${__dirname}/src/data/`,
-        //     },
-        // },
+        {
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {
+                // The property ID; the tracking code won't be generated without it
+                trackingId: 'UA-125533843-1',
+                // Defines where to place the tracking script - `true` in the head and `false` in the body
+                head: false,
+                // Setting this parameter is optional
+                anonymize: true,
+                // Setting this parameter is also optional
+                // Defers execution of google analytics script after page load
+                defer: false,
+                // Any additional optional fields
+                sampleRate: 5,
+                siteSpeedSampleRate: 10,
+            },
+        },
 
     ],
 };
